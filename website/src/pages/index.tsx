c89722568e7eebb78d1e63be6b8f33f350a57bc7
Liviu Ionescu <ilg@livius.net>
2025-01-21 15:21:15 +0200

    website: re-generate commons

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -34,7 +34,7 @@ function HomepageHeader() {
         <p className="hero__subtitle">{siteConfig.tagline}</p>
 
         <div className={styles.installWithCopy}>
-          <InstallWithCopy>xpm install @xpack-dev-tools/aarch64-none-elf-gcc@{customField('releaseVersion')} --verbose</InstallWithCopy>
+          <InstallWithCopy>xpm install @xpack-dev-tools/aarch64-none-elf-gcc@14.2.1-1.1.1 --verbose</InstallWithCopy>
         </div>
 
       </div>
